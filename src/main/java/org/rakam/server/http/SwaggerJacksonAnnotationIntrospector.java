7e7e0a40ada43b30c22e7a81bd67fd22d5e85da8
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-03-09 20:52:24 +0200

    add required marker for @ApiParam

--- src/main/java/org/rakam/server/http/SwaggerJacksonAnnotationIntrospector.java
+++ src/main/java/org/rakam/server/http/SwaggerJacksonAnnotationIntrospector.java
@@ -2,8 +2,13 @@ package org.rakam.server.http;
 
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.introspect.Annotated;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.rakam.server.http.annotations.ApiParam;
+
+import javax.xml.bind.annotation.XmlElement;
 
 public class SwaggerJacksonAnnotationIntrospector extends AnnotationIntrospector {
     @Override
@@ -19,5 +24,38 @@ public class SwaggerJacksonAnnotationIntrospector extends AnnotationIntrospector
         } else {
             return super.findImplicitPropertyName(member);
         }
+    }
+
+    @Override
+    public Boolean hasRequiredMarker(AnnotatedMember m) {
+        ApiParam apiParam = m.getAnnotation(ApiParam.class);
+        if(apiParam !=null) {
+            return apiParam.required();
+        }
+
+        ApiModelProperty ann = m.getAnnotation(ApiModelProperty.class);
+        if (ann != null) {
+            return ann.required();
+        }
+        XmlElement elem = m.getAnnotation(XmlElement.class);
+        if (elem != null) {
+            if (elem.required()) {
+                return true;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String findPropertyDescription(Annotated a) {
+        ApiModel model = a.getAnnotation(ApiModel.class);
+        if (model != null && !"".equals(model.description())) {
+            return model.description();
+        }
+        ApiModelProperty prop = a.getAnnotation(ApiModelProperty.class);
+        if (prop != null) {
+            return prop.value();
+        }
+        return null;
     }
 }
