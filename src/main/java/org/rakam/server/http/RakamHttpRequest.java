23d7cc8593c95ff1c31d016d8c8836b8239ad707
Burak Emre Kabakcı <emrekabakci@gmail.com>
2018-05-07 13:52:12 +0300

    public

--- src/main/java/org/rakam/server/http/RakamHttpRequest.java
+++ src/main/java/org/rakam/server/http/RakamHttpRequest.java
@@ -171,7 +171,7 @@ public class RakamHttpRequest
         return qs.path();
     }
 
-    private void addResponseCookie(Cookie cookie) {
+    public void addResponseCookie(Cookie cookie) {
         if (responseCookies != null) {
             responseCookies = new ArrayList<>();
         }
@@ -179,7 +179,7 @@ public class RakamHttpRequest
         responseCookies.add(cookie);
     }
 
-    private void addResponseHeader(String key, String value) {
+    public void addResponseHeader(String key, String value) {
         if (responseHeaders != null) {
             responseHeaders = new HashMap<>();
         }
